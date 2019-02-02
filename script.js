bb246b52bbe7b996771ab29d2f201310b072a3e5
paulinavita <paulinadavita@gmail.com>
2019-02-02 22:00:30 +0700

    small rev

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ var button = document.querySelector('button');
 var words = document.querySelector(".word-show-text");
 var scoreDiv = document.querySelector(".top-score-text");
 var timerDiv = document.querySelector('.timer-box-text');
-
 var input = document.getElementById('inputLetter')
 var points = 0;
-var spans = ''
-var typed = ''
 var seconds = 61;
 var arrayWords = ["ACTUALISM","ADD","AFFLUENT","ALIZARINE","ANOXIC","ANTARTHRITIC","APHAREUS","ARCHIBALD","ART","ASCENDING","ASCITIC",
 "ASININITY","BABY","BALL","BALSAMY","BANK","BANNISTER","BAR","BAY","BEE","BEMUSEMENT","BENEMID","BET","BIOSCOPY","BOW","BRAG","BUTYLENE",
@@ -22,7 +19,6 @@ var arrayWords = ["ACTUALISM","ADD","AFFLUENT","ALIZARINE","ANOXIC","ANTARTHRITI
 "SUM","SYSSARCOSIS","TERM","TEXT","THIN","TRANSLATE","UNANGRILY","UNDESPISED","UNDEVOTED","UNFRICATIVE","UNGRATIFIED","UNIT","UNPRATING",
 "UNSHEDDING","VAIN","VANZETTI","VEIN","VULPICIDE","WALL","WARN","WAY","WEIRDNESS","WRAP", "ZWITTERIONIC"]
 var imagesLists = ["img/80sbackground.jpg","img/80sbackground_shift.jpg"]
-// const isStorage = 'undefined' !== typeof localStorage
 var eightiesImages = document.getElementById("eightiesBackground");
 var arrayHighLists = [];
 var arrayHighListsName =[];
@@ -79,10 +75,6 @@ function decrementTime() {
     seconds--;
     timestamp.innerHTML = seconds;
     if (seconds === 0) {
-      // if (isStorage && localStorage.getItem(points)) {
-      //   var highestScoreSaved = document.querySelector(".highest-score-points")
-      //   elements.highestScoreSaved = localStorage.getItem(points)
-      // }
       seconds--;
       alert("Thank you for playing! Your points : " + points + 'You can check if you are on the toplist');
       window.location = 'index.html' // 
@@ -95,7 +87,6 @@ function decrementTime() {
       button.disabled = false;	
     }
 }, 1000);
-// isStorage && localStorage.setItem(points, elements.highestScoreSaved);
 }
 
 //kalo di tekan enter
@@ -117,71 +108,5 @@ function grade() {
       scoreDiv.innerHTML = points
       randomWords()
     }
-    arrayHighLists.push(points) 
-    arrayHighListsName.push(document.querySelector(".name-box-text").innerHTML)
-}
-
   
-
-//membuat highscore (still not working)
-/* 
-push input nama ke array
-push points ke array
-kalo gak ada di highscore obj, key namaorang, maka buat key tsb
-if (!highscoreObj[inputnama]) {
-  highscoreObj[inputnama] = {}
-  highscoreObj[inputnama].nama = inputnama[i]
-  highscoreObj[inputnama].points = points[i]
 }
-
-*/
-
-
-
-
-// var highscoreList=[
-//   {score:2, name:'John', highscoreLine:[3,1,4,5,4,3,2]},
-//   {score:5, name:'John B', highscoreLine:[3,1,4,5,2,3,3]},
-//   {score:10, name:'John C', highscoreLine:[3,1,4,5,7,6,8]},
-//   {score:4, name:'John', highscoreLine:[3,1,4,5,9,8,7]}
-// ];
-
-// highscoreList.sort(function(a,b){return a.score < b.score});
-// console.log(highscoreList)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//panggil checker kalau enter di pencet
-// function checker (e) {
-//   var hitung = 0
-//   for (j = 0; i < words.innerHTML.length ;j++) {
-//     if (words.innerHTML[j] === input[j].value) {
-//       hitung++
-//     }
-//     if (hitung === words.innerHTML.length) {
-//       points += 1
-//       scoreDiv.innerHTML = points
-//       document.removeEventListener("keydown", checker, false);
-//       setTimeout(function(){
-//       words.className = "words"; // restart the classes
-//       randomWords(); // give another word
-//       document.removeEventListener("keydown", checker, false);
-
-//     }, 400);
-//   }
-// }
-// }
-
-// document.addEv
